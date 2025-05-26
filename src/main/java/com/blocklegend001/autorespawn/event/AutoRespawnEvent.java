99ea791fc6d0ffed06ce2ad722eeba67b9275e53
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-26 19:21:26 +0200

    Added config

--- src/main/java/com/blocklegend001/autorespawn/event/AutoRespawnEvent.java
+++ src/main/java/com/blocklegend001/autorespawn/event/AutoRespawnEvent.java
@@ -1,6 +1,7 @@
 package com.blocklegend001.autorespawn.event;
 
 import com.blocklegend001.autorespawn.AutoRespawn;
+import com.blocklegend001.autorespawn.ModConfigs;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -29,7 +30,7 @@ public class AutoRespawnEvent {
             wasDead = true;
         }
 
-        if (System.currentTimeMillis() - deathTime >= 2000) {
+        if (System.currentTimeMillis() - deathTime >= ModConfigs.respawnDelay) {
             if (client.player != null) {
                 client.player.respawn();
                 client.setScreen(null);
