ff33090895f9ec2e519191280b2932b697c0385f
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-26 19:19:22 +0200

    Added config

--- src/main/java/com/blocklegend001/autorespawn/AutoRespawn.java
+++ src/main/java/com/blocklegend001/autorespawn/AutoRespawn.java
@@ -2,8 +2,13 @@ package com.blocklegend001.autorespawn;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import org.slf4j.Logger;
 
 @Mod(AutoRespawn.MODID)
@@ -14,6 +19,15 @@ public class AutoRespawn {
 
     public AutoRespawn(FMLJavaModLoadingContext context) {
         LOGGER.info("AutoRespawn Loaded!");
+        IEventBus modEventBus = context.getModEventBus();
+        context.registerConfig(ModConfig.Type.COMMON, ModConfigs.SPEC, "autorespawn.toml");
+        ModConfigs.loadConfig(ModConfigs.SPEC, FMLPaths.CONFIGDIR.get().resolve("autorespawn.toml"));
+        modEventBus.addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void setup(final FMLCommonSetupEvent event) {
+        ModConfigs.loadConfig(ModConfigs.SPEC, FMLPaths.CONFIGDIR.get().resolve("autorespawn.toml"));
     }
 }
