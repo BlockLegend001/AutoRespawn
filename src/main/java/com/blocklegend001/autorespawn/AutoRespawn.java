f7e731bb192f017436d052e6ddb58b1aba1af512
BlockLegend001 <rosarioarcamone@gmail.com>
2025-05-26 22:07:11 +0200

    Added config

--- src/main/java/com/blocklegend001/autorespawn/AutoRespawn.java
+++ src/main/java/com/blocklegend001/autorespawn/AutoRespawn.java
@@ -2,7 +2,9 @@ package com.blocklegend001.autorespawn;
 
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 @Mod(AutoRespawn.MODID)
@@ -10,7 +12,8 @@ public class AutoRespawn {
     public static final String MODID = "autorespawn";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public AutoRespawn(IEventBus modEventBus) {
+    public AutoRespawn(IEventBus modEventBus, ModContainer modContainer) {
         LOGGER.info("AutoRespawn Loaded!");
+        modContainer.registerConfig(ModConfig.Type.COMMON, ModConfigs.SPEC);
     }
 }
