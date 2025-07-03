7acffbb396e8be397246cbdc591fa244faba50fb
BlockLegend001 <rosarioarcamone@gmail.com>
2025-07-03 10:41:24 +0200

    Added 1.21.7-Fabric

---
+++ src/main/java/com/blocklegend001/autorespawn/AutoRespawn.java
@@ -0,0 +1,18 @@
+package com.blocklegend001.autorespawn;
+
+import com.blocklegend001.autorespawn.config.ModConfig;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoRespawn implements ModInitializer {
+    public static final String MOD_ID = "autorespawn";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("AutoRespawn Loaded!");
+        ModConfig.registerConfigs();
+    }
+}
